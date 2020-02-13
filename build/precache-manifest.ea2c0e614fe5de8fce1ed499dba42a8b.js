6ecb6b4ec08f9c16f9ac95396ccf6df09874e6b6
Yaroslav <ssilvias13@gmail.com>
2020-02-13 19:25:39 +0200

    please work

---
+++ build/precache-manifest.ea2c0e614fe5de8fce1ed499dba42a8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa58f2cc885e650cc2ade325c8a6c374",
+    "url": "/list-form/build/index.html"
+  },
+  {
+    "revision": "293a0ff6b648c511fa03",
+    "url": "/list-form/build/static/css/main.f1226403.chunk.css"
+  },
+  {
+    "revision": "60e473d76bae4cf17bdc",
+    "url": "/list-form/build/static/js/2.1b564e2f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/list-form/build/static/js/2.1b564e2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "293a0ff6b648c511fa03",
+    "url": "/list-form/build/static/js/main.56ab8d7a.chunk.js"
+  },
+  {
+    "revision": "6e4dcdce57d734d4d008",
+    "url": "/list-form/build/static/js/runtime-main.f14f127d.js"
+  }
+]);
